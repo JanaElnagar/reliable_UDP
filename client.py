617cb0e9e7f5588e52ef30ac66c4735d0087fe8b
Jana Essam <janaelnagar18@gmail.com>
2024-05-10 10:17:18 +0300

    Implement handshake

--- client.py
+++ client.py
@@ -1,34 +1,68 @@
-from ReliableUDPSocket import ReliableUDPSocket
-from ReliableUDPSocket import FLAG_SYN, FLAG_SYNACK, FLAG_ACK, FLAG_FIN, FLAG_DATA
+import socket
+import json
+import time
 
-class Client:
-    def __init__(self, server_address):
-        self.socket = ReliableUDPSocket(server_address)
 
-    def send_http_request(self, method, url, headers={}, data=b""):
-        request = f"{method} {url} HTTP/1.0\r\n"
-        for header, value in headers.items():
-            request += f"{header}: {value}\r\n"
-        request += "\r\n"
-        request += data.decode()
-        packets = [request[i:i + 1024] for i in range(0, len(request), 1024)]
-        for i, packet in enumerate(packets):
-            self.socket.send_packet(packet.encode(), FLAG_DATA)
-            self.socket.handle_timeout()
+class UDPTCP_Client:
+    def __init__(self, server_address, server_port, client_address, client_port):
+        self.server_address = server_address
+        self.server_port = server_port
+        self.client_address = client_address  # Client's IP address
+        self.client_port = client_port  # Client's source port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((self.client_address, self.client_port))  # Bind to client's source IP address and port
+        self.sequence_number = 0
+        self.ack_number = 0
 
-    def process_http_response(self, data):
-        response_lines = data.decode().split("\r\n")
-        status_code = response_lines[0].split()[1]
-        headers = {}
-        for line in response_lines[1:]:
-            if line:
-                key, value = line.split(": ")
-                headers[key] = value
-        # Process response data and headers
+    def handshake(self):
+        # Send SYN packet with client's IP address and source port
+        syn_packet = {'type': 'SYN', 'sequence_number': self.sequence_number, 'client_ip': self.client_address, 'client_port': self.client_port}
+        self.socket.sendto(json.dumps(syn_packet).encode(), (self.server_address, self.server_port))
+
+        # Receive SYN-ACK packet
+        syn_ack, _ = self.socket.recvfrom(1024)
+        syn_ack_packet = json.loads(syn_ack.decode())
+        if syn_ack_packet['type'] == 'SYN-ACK':
+            self.sequence_number = syn_ack_packet['ack_number']
+            self.ack_number = syn_ack_packet['sequence_number'] + 1
+            print("client received SYN-ACK")
+
+            # Send ACK packet
+            ack_packet = {'type': 'ACK', 'sequence_number': self.sequence_number,'ack_number': self.ack_number, 'client_ip': self.client_address, 'client_port': self.client_port}
+            self.socket.sendto(json.dumps(ack_packet).encode(), (self.server_address, self.server_port))
+            print("Client sent ACK")
+            return True
+        else:
+            return False
+
+    def send_data(self, data):
+        # Send data packet
+        data_packet = {'type': 'DATA', 'sequence_number': self.sequence_number, 'data': data}
+        self.socket.sendto(json.dumps(data_packet).encode(), (self.server_address, self.server_port))
+
+        # Receive ACK packet
+        ack, _ = self.socket.recvfrom(1024)
+        ack_packet = json.loads(ack.decode())
+        self.sequence_number = ack_packet['ack_number']
+        self.ack_number += len(ack_packet)
+        if ack_packet['type'] == 'ACK' :
+            self.sequence_number += 1
+            return True
+        else:
+            return False
+
+    def stop(self):
+        self.socket.close()
+
+    def start(self):
+        if self.handshake():
+            # Send HTTP request
+            http_request = {'method': 'GET', 'url': '/example'}
+            self.send_data(json.dumps(http_request))
+        else:
+            print("Failed to establish connection")
+
 
 if __name__ == "__main__":
-    server_address = ("127.0.0.1", 5000)  # Replace with server address and port
-    client = Client(server_address)
-    client.send_http_request("GET", "/index.html")
-    data, _ = client.socket.recvfrom(1024)
-    client.process_http_response(data)
+    client = UDPTCP_Client('localhost', 8000, 'localhost', 50506)  # Pass client's IP address and source port as arguments
+    client.start()
--- server.py
+++ server.py
@@ -1,28 +1,67 @@
-from ReliableUDPSocket import ReliableUDPSocket
-from ReliableUDPSocket import FLAG_SYN, FLAG_SYNACK, FLAG_ACK, FLAG_FIN, FLAG_DATA
-import struct
+import socket
+import json
 
-class Server:
-    def __init__(self):
-        self.socket = ReliableUDPSocket(("0.0.0.0", 5000))  # Listen on all interfaces
+class UDPTCP_Server:
+    def __init__(self, server_address, server_port):
+        self.server_address = server_address
+        self.server_port = server_port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((self.server_address, self.server_port))
+        self.sequence_number = 0
+        self.ack_number = 0
 
-    def handle_client(self, client_address):
-        while True:
-            self.socket.handle_timeout()
-            data, _ = self.socket.recvfrom(1024)
-            if not data:
-                break
-            # Process HTTP request (parse data, generate response)
-            response = "HTTP/1.0 200 OK\r\nContent-Length: 10\r\n\r\nHello World"
-            self.socket.send_packet(response.encode(), FLAG_DATA)
-            self.socket.handle_timeout()
-            # Wait for FIN and send ACK
-            data, _ = self.socket.recvfrom(1024)
-            if data and struct.unpack("!I", data[:4])[0] & FLAG_FIN:
-                self.socket.send_packet(b"", FLAG_ACK)
-                break
+    def handshake(self):
+        # Receive SYN packet
+        syn, client_address = self.socket.recvfrom(1024)
+        syn_packet = json.loads(syn.decode())
+        if syn_packet['type'] == 'SYN':
+            self.ack_number = syn_packet['sequence_number'] + 1
+            print("server received SYN")
+
+            # Send SYN-ACK packet with client's IP address and source port
+            syn_ack_packet = {'type': 'SYN-ACK', 'sequence_number': self.sequence_number, 'ack_number': self.ack_number, 'client_ip': syn_packet['client_ip'], 'client_port': syn_packet['client_port']}
+            self.socket.sendto(json.dumps(syn_ack_packet).encode(), client_address)
+
+            # Receive ACK packet
+            #print("about to receive ACK")
+            ack, _ = self.socket.recvfrom(1024)
+            #print("received ACK packet (undecoded)")
+            ack_packet = json.loads(ack.decode())
+            print(ack_packet['sequence_number'])
+            print(self.ack_number)
+            if ack_packet['type'] == 'ACK' and ack_packet['sequence_number'] == self.ack_number:
+                print("Server received ACK")
+               # self.sequence_number += 1  # update the sequence number
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def receive_data(self):
+        # Receive data packet
+        data, _ = self.socket.recvfrom(1024)
+        data_packet = json.loads(data.decode())
+        self.ack_number += len(data_packet)
+        # Send ACK packet
+        ack_packet = {'type': 'ACK', 'sequence_number': self.sequence_number, 'ack_number':self.ack_number}
+        self.socket.sendto(json.dumps(ack_packet).encode(), (data_packet['client_ip'], data_packet['client_port']))
+
+        return data_packet['data']
+
+    def handle_request(self, request):
+        # Handle HTTP request
+        print("Received HTTP request:", request)
+        # For simplicity, just print the request for now
+
+    def start(self):
+        if self.handshake():
+            while True:
+                request = self.receive_data()
+                self.handle_request(request)
+        else:
+            print("Failed to establish connection")
 
 if __name__ == "__main__":
-    server = Server()
-    while True:
-        server.handle_client(None)
+    server = UDPTCP_Server('localhost', 8000)
+    server.start()
