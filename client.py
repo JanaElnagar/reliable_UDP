096e280c6dfbf7c9ee06d7a8987d327d50b2d004
Jana Essam <janaelnagar18@gmail.com>
2024-05-11 18:48:15 +0300

    add checksum

--- client.py
+++ client.py
@@ -1,34 +1,109 @@
-from ReliableUDPSocket import ReliableUDPSocket
-from ReliableUDPSocket import FLAG_SYN, FLAG_SYNACK, FLAG_ACK, FLAG_FIN, FLAG_DATA
+import socket
+import json
+import random
+import binascii
+import zlib
 
-class Client:
-    def __init__(self, server_address):
-        self.socket = ReliableUDPSocket(server_address)
 
-    def send_http_request(self, method, url, headers={}, data=b""):
-        request = f"{method} {url} HTTP/1.0\r\n"
-        for header, value in headers.items():
-            request += f"{header}: {value}\r\n"
-        request += "\r\n"
-        request += data.decode()
-        packets = [request[i:i + 1024] for i in range(0, len(request), 1024)]
-        for i, packet in enumerate(packets):
-            self.socket.send_packet(packet.encode(), FLAG_DATA)
-            self.socket.handle_timeout()
+class UDPTCP_Client:
+    def __init__(self, server_address, server_port, client_address, client_port):
+        self.server_address = server_address
+        self.server_port = server_port
+        self.client_address = client_address  # Client's IP address
+        self.client_port = client_port  # Client's source port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((self.client_address, self.client_port))  # Bind to client's source IP address and port
+        self.sequence_number = random.randint(0, 1000)  # Random initial sequence number
+        self.ack_number = 0
+        self.flags = '00000000'  # Initialize flags
 
-    def process_http_response(self, data):
-        response_lines = data.decode().split("\r\n")
-        status_code = response_lines[0].split()[1]
-        headers = {}
-        for line in response_lines[1:]:
-            if line:
-                key, value = line.split(": ")
-                headers[key] = value
-        # Process response data and headers
+    def display(self, packet):
+        print("type: " + packet['type'])
+        print("seq_num: " + str(packet['sequence_number']))
+        print("ack_num: " + str(packet['ack_number']))
+        print("flags: " + packet['flags'])
+        print("------------------------")
+
+    def display_self(self):
+        print("current state")
+        print("seq_num: " + str(self.sequence_number))
+        print("ack_num: " + str(self.ack_number))
+        print("flags: " + self.flags)
+        print("------------------------")
+
+    def calculate_checksum(self, data):
+        checksum = zlib.crc32(data.encode())
+        return checksum
+
+    def handshake(self):
+        # Send SYN packet with client's IP address and source port
+        self.flags = '00000010'  # Set SYN flag
+        syn_packet = {'type': 'SYN', 'sequence_number': self.sequence_number, 'ack_number': None,
+                      'client_ip': self.client_address, 'client_port': self.client_port, 'flags': self.flags}
+        self.socket.sendto(json.dumps(syn_packet).encode(), (self.server_address, self.server_port))
+        self.display(syn_packet)
+
+        # Receive SYN-ACK packet
+        syn_ack, _ = self.socket.recvfrom(1024)
+        syn_ack_packet = json.loads(syn_ack.decode())
+        if syn_ack_packet['type'] == 'SYN-ACK' and syn_ack_packet['flags'][6] == '1' and syn_ack_packet['flags'][
+            3] == '1':
+            self.display(syn_ack_packet)
+            self.sequence_number = syn_ack_packet['ack_number']
+            self.ack_number = syn_ack_packet['sequence_number'] + 1
+
+            # Send ACK packet
+            self.flags = '00010000'  # Set ACK flag
+            ack_packet = {'type': 'ACK', 'sequence_number': self.sequence_number, 'ack_number': self.ack_number,
+                          'client_ip': self.client_address, 'client_port': self.client_port, 'flags': self.flags}
+
+            # Calculate checksum and include it in the packet
+            ack_packet['checksum'] = self.calculate_checksum(json.dumps(ack_packet))
+
+            self.socket.sendto(json.dumps(ack_packet).encode(), (self.server_address, self.server_port))
+            print("Client sent ACK")
+            # self.ack_number += 1
+            return True
+        else:
+            return False
+
+    def send_data(self, data):
+        # Send data packet
+        self.flags = '00000000'  # Reset flags
+        self.sequence_number += 1
+        data_packet = {'type': 'DATA', 'sequence_number': self.sequence_number, 'ack_number': self.ack_number,
+                       'client_ip': self.client_address, 'client_port': self.client_port, 'data': data,
+                       'flags': self.flags}
+
+        # Calculate checksum and include it in the packet
+        data_packet['checksum'] = self.calculate_checksum(json.dumps(data_packet))
+
+        self.socket.sendto(json.dumps(data_packet).encode(), (self.server_address, self.server_port))
+
+        # Receive ACK packet
+        ack, _ = self.socket.recvfrom(1024)
+        ack_packet = json.loads(ack.decode())
+        self.display(ack_packet)
+        if ack_packet['type'] == 'ACK' and ack_packet['flags'][3] == '1':
+            self.sequence_number += len(data)  # Increment sequence number by length of data
+            self.ack_number = data_packet['sequence_number'] + 1
+            return True
+        else:
+            return False
+
+    def stop(self):
+        self.socket.close()
+
+    def start(self):
+        if self.handshake():
+            # Send HTTP request
+            http_request = {'method': 'GET', 'url': '/example'}
+            self.send_data(json.dumps(http_request))
+        else:
+            print("Failed to establish connection")
+
 
 if __name__ == "__main__":
-    server_address = ("127.0.0.1", 5000)  # Replace with server address and port
-    client = Client(server_address)
-    client.send_http_request("GET", "/index.html")
-    data, _ = client.socket.recvfrom(1024)
-    client.process_http_response(data)
+    client = UDPTCP_Client('localhost', 8000, 'localhost',
+                           50506)  # Pass client's IP address and source port as arguments
+    client.start()
--- server.py
+++ server.py
@@ -1,28 +1,132 @@
-from ReliableUDPSocket import ReliableUDPSocket
-from ReliableUDPSocket import FLAG_SYN, FLAG_SYNACK, FLAG_ACK, FLAG_FIN, FLAG_DATA
-import struct
+import socket
+import json
+import random
+import binascii
+import zlib
 
-class Server:
-    def __init__(self):
-        self.socket = ReliableUDPSocket(("0.0.0.0", 5000))  # Listen on all interfaces
 
-    def handle_client(self, client_address):
-        while True:
-            self.socket.handle_timeout()
-            data, _ = self.socket.recvfrom(1024)
-            if not data:
-                break
-            # Process HTTP request (parse data, generate response)
-            response = "HTTP/1.0 200 OK\r\nContent-Length: 10\r\n\r\nHello World"
-            self.socket.send_packet(response.encode(), FLAG_DATA)
-            self.socket.handle_timeout()
-            # Wait for FIN and send ACK
-            data, _ = self.socket.recvfrom(1024)
-            if data and struct.unpack("!I", data[:4])[0] & FLAG_FIN:
-                self.socket.send_packet(b"", FLAG_ACK)
-                break
+class UDPTCP_Server:
+    def __init__(self, server_address, server_port):
+        self.server_address = server_address
+        self.server_port = server_port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((self.server_address, self.server_port))
+        self.sequence_number = random.randint(0, 1000)  # Random initial sequence number
+        self.ack_number = 0
+        self.flags = '00000000'  # Initialize flags
+
+    def display(self, packet):
+        print("type: " + packet['type'])
+        print("seq_num: " + str(packet['sequence_number']))
+        print("ack_num: " + str(packet['ack_number']))
+        print("flags: " + packet['flags'])
+        print("------------------------")
+
+    def display_self(self):
+        print("current state")
+        print("seq_num: " + str(self.sequence_number))
+        print("ack_num: " + str(self.ack_number))
+        print("flags: " + self.flags)
+        print("------------------------")
+
+    def calculate_checksum(self, data):
+        checksum = zlib.crc32(data.encode())
+        return checksum
+
+    def verify_checksum(self, packet):
+        received_checksum = packet.get('checksum')
+        del packet['checksum']  # Remove checksum from the packet for checksum calculation
+        calculated_checksum = self.calculate_checksum(json.dumps(packet))
+        return received_checksum == calculated_checksum
+
+    def simulate_false_checksum(self, packet):
+        # Simulate a false checksum by modifying the received packet's checksum
+        packet['checksum'] = random.randint(0, 2 ** 32 - 1)  # Generate a random checksum value
+        return packet
+
+    def handshake(self):
+        # Receive SYN packet
+        syn, client_address = self.socket.recvfrom(1024)
+        syn_packet = json.loads(syn.decode())
+        if syn_packet['type'] == 'SYN' and syn_packet['flags'][6] == '1':
+            self.ack_number = syn_packet['sequence_number'] + 1
+            self.display(syn_packet)
+
+            # Send SYN-ACK packet with client's IP address and source port
+            self.flags = '00010010'  # Set SYN and ACK flags
+            syn_ack_packet = {'type': 'SYN-ACK', 'sequence_number': self.sequence_number, 'ack_number': self.ack_number,
+                              'client_ip': syn_packet['client_ip'], 'client_port': syn_packet['client_port'],
+                              'flags': self.flags}
+
+            # Calculate checksum and include it in the packet
+            syn_ack_packet['checksum'] = self.calculate_checksum(json.dumps(syn_ack_packet))
+
+            self.socket.sendto(json.dumps(syn_ack_packet).encode(), client_address)
+
+            # Receive ACK packet
+            ack, client_address = self.socket.recvfrom(1024)
+            ack_packet = json.loads(ack.decode())
+            self.display(ack_packet)
+            if ack_packet['type'] == 'ACK' and ack_packet['sequence_number'] == self.ack_number and ack_packet['flags'][
+                3] == '1':
+                self.sequence_number = ack_packet['ack_number']
+                self.ack_number = ack_packet['sequence_number'] + 1
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def receive_data(self):
+        # Receive data packet
+        data, client_address = self.socket.recvfrom(1024)
+        data_packet = json.loads(data.decode())
+
+        #data_packet = self.simulate_false_checksum(data_packet)    # for checksum testing
+
+        # Verify checksum
+        if self.verify_checksum(data_packet):
+            # Check if the sequence number is as expected
+            if data_packet['sequence_number'] == self.ack_number:
+                self.display(data_packet)
+                self.ack_number = data_packet['sequence_number'] + len(
+                    data_packet['data'])  # Update acknowledgment number
+                self.sequence_number = data_packet['ack_number']
+                # Send ACK packet
+                self.flags = '00010000'  # Set ACK flag
+                ack_packet = {'type': 'ACK', 'sequence_number': self.sequence_number, 'ack_number': self.ack_number,
+                              'client_ip': data_packet['client_ip'], 'client_port': data_packet['client_port'],
+                              'flags': self.flags}
+
+                # Calculate checksum and include it in the ACK packet
+                ack_packet['checksum'] = self.calculate_checksum(json.dumps(ack_packet))
+
+                self.socket.sendto(json.dumps(ack_packet).encode(), client_address)
+                return data_packet['data']
+            else:
+                # Handle out of order
+                print("retry")
+                return False
+        else:
+            print("Checksum verification failed. Dropping packet.")
+            return False
+
+
+    def handle_request(self, request):
+        # Handle HTTP request
+        print("Received HTTP request:", request)
+        # For simplicity, just print the request for now
+
+    def start(self):
+
+        if self.handshake():
+            while True:
+                request = self.receive_data()
+                self.handle_request(request)
+        else:
+            print("Failed to establish connection")
+
 
 if __name__ == "__main__":
-    server = Server()
-    while True:
-        server.handle_client(None)
+    server = UDPTCP_Server('localhost', 8000)
+    server.start()
