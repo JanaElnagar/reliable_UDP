e391c7223f694da00f02a42df1231b6699e64729
Jana Essam <janaelnagar18@gmail.com>
2024-05-11 07:54:59 +0300

    add TCP flags

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 import socket
 import json
-import time
-
+import random
 
 class UDPTCP_Client:
     def __init__(self, server_address, server_port, client_address, client_port):
@@ -11,42 +10,64 @@ class UDPTCP_Client:
         self.client_port = client_port  # Client's source port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind((self.client_address, self.client_port))  # Bind to client's source IP address and port
-        self.sequence_number = 0
+        self.sequence_number = random.randint(0, 1000)  # Random initial sequence number
         self.ack_number = 0
+        self.flags = '00000000'  # Initialize flags
+
+    def display(self, packet):
+        print("type: "+packet['type'])
+        print("seq_num: "+str(packet['sequence_number']))
+        print("ack_num: "+str(packet['ack_number']))
+        print("flags: "+packet['flags'])
+        print("------------------------")
+
+    def display_self(self):
+        print("current state")
+        print("seq_num: "+str(self.sequence_number))
+        print("ack_num: "+str(self.ack_number))
+        print("flags: "+self.flags)
+        print("------------------------")
 
     def handshake(self):
         # Send SYN packet with client's IP address and source port
-        syn_packet = {'type': 'SYN', 'sequence_number': self.sequence_number, 'client_ip': self.client_address, 'client_port': self.client_port}
+        self.flags = '00000010'  # Set SYN flag
+        syn_packet = {'type': 'SYN', 'sequence_number': self.sequence_number, 'ack_number': None, 'client_ip': self.client_address, 'client_port': self.client_port, 'flags':self.flags}
         self.socket.sendto(json.dumps(syn_packet).encode(), (self.server_address, self.server_port))
+        self.display(syn_packet)
 
         # Receive SYN-ACK packet
         syn_ack, _ = self.socket.recvfrom(1024)
         syn_ack_packet = json.loads(syn_ack.decode())
-        if syn_ack_packet['type'] == 'SYN-ACK':
+        if syn_ack_packet['type'] == 'SYN-ACK' and syn_ack_packet['flags'][6] == '1' and syn_ack_packet['flags'][3] == '1':
+            self.display(syn_ack_packet)
             self.sequence_number = syn_ack_packet['ack_number']
             self.ack_number = syn_ack_packet['sequence_number'] + 1
-            print("client received SYN-ACK")
 
             # Send ACK packet
-            ack_packet = {'type': 'ACK', 'sequence_number': self.sequence_number,'ack_number': self.ack_number, 'client_ip': self.client_address, 'client_port': self.client_port}
+            self.flags = '00010000'  # Set ACK flag
+            ack_packet = {'type': 'ACK', 'sequence_number': self.sequence_number, 'ack_number': self.ack_number, 'client_ip': self.client_address, 'client_port': self.client_port, 'flags':self.flags}
             self.socket.sendto(json.dumps(ack_packet).encode(), (self.server_address, self.server_port))
             print("Client sent ACK")
+            #self.ack_number += 1
             return True
         else:
             return False
 
     def send_data(self, data):
         # Send data packet
-        data_packet = {'type': 'DATA', 'sequence_number': self.sequence_number, 'data': data}
+        self.flags = '00000000'  # Reset flags
+        self.sequence_number += 1
+        data_packet = {'type': 'DATA', 'sequence_number': self.sequence_number, 'ack_number':self.ack_number, 'data': data, 'flags': self.flags}
+
         self.socket.sendto(json.dumps(data_packet).encode(), (self.server_address, self.server_port))
 
         # Receive ACK packet
         ack, _ = self.socket.recvfrom(1024)
         ack_packet = json.loads(ack.decode())
-        self.sequence_number = ack_packet['ack_number']
-        self.ack_number += len(ack_packet)
-        if ack_packet['type'] == 'ACK' :
-            self.sequence_number += 1
+        self.display(ack_packet)
+        if ack_packet['type'] == 'ACK' and ack_packet['flags'][3] == '1':
+            self.sequence_number += len(data)  # Increment sequence number by length of data
+            self.ack_number = data_packet['sequence_number'] + 1
             return True
         else:
             return False
@@ -61,7 +82,6 @@ class UDPTCP_Client:
             self.send_data(json.dumps(http_request))
         else:
             print("Failed to establish connection")
-
 
 if __name__ == "__main__":
     client = UDPTCP_Client('localhost', 8000, 'localhost', 50506)  # Pass client's IP address and source port as arguments
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import json
+import random
 
 class UDPTCP_Server:
     def __init__(self, server_address, server_port):
@@ -7,31 +8,44 @@ class UDPTCP_Server:
         self.server_port = server_port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind((self.server_address, self.server_port))
-        self.sequence_number = 0
+        self.sequence_number = random.randint(0, 1000)  # Random initial sequence number
         self.ack_number = 0
+        self.flags = '00000000'  # Initialize flags
+
+    def display(self, packet):
+        print("type: "+packet['type'])
+        print("seq_num: "+str(packet['sequence_number']))
+        print("ack_num: "+str(packet['ack_number']))
+        print("flags: "+packet['flags'])
+        print("------------------------")
+
+    def display_self(self):
+        print("current state")
+        print("seq_num: "+str(self.sequence_number))
+        print("ack_num: "+str(self.ack_number))
+        print("flags: "+self.flags)
+        print("------------------------")
 
     def handshake(self):
         # Receive SYN packet
         syn, client_address = self.socket.recvfrom(1024)
         syn_packet = json.loads(syn.decode())
-        if syn_packet['type'] == 'SYN':
+        if syn_packet['type'] == 'SYN' and syn_packet['flags'][6] == '1':
             self.ack_number = syn_packet['sequence_number'] + 1
-            print("server received SYN")
+            self.display(syn_packet)
 
             # Send SYN-ACK packet with client's IP address and source port
-            syn_ack_packet = {'type': 'SYN-ACK', 'sequence_number': self.sequence_number, 'ack_number': self.ack_number, 'client_ip': syn_packet['client_ip'], 'client_port': syn_packet['client_port']}
+            self.flags = '00010010'  # Set SYN and ACK flags
+            syn_ack_packet = {'type': 'SYN-ACK', 'sequence_number': self.sequence_number, 'ack_number': self.ack_number, 'client_ip': syn_packet['client_ip'], 'client_port': syn_packet['client_port'], 'flags':self.flags}
             self.socket.sendto(json.dumps(syn_ack_packet).encode(), client_address)
 
             # Receive ACK packet
-            #print("about to receive ACK")
             ack, _ = self.socket.recvfrom(1024)
-            #print("received ACK packet (undecoded)")
             ack_packet = json.loads(ack.decode())
-            print(ack_packet['sequence_number'])
-            print(self.ack_number)
-            if ack_packet['type'] == 'ACK' and ack_packet['sequence_number'] == self.ack_number:
-                print("Server received ACK")
-               # self.sequence_number += 1  # update the sequence number
+            self.display(ack_packet)
+            if ack_packet['type'] == 'ACK' and ack_packet['sequence_number'] == self.ack_number and ack_packet['flags'][3]=='1':
+                self.sequence_number = ack_packet['ack_number']
+                self.ack_number = ack_packet['sequence_number'] + 1
                 return True
             else:
                 return False
@@ -42,12 +56,21 @@ class UDPTCP_Server:
         # Receive data packet
         data, _ = self.socket.recvfrom(1024)
         data_packet = json.loads(data.decode())
-        self.ack_number += len(data_packet)
-        # Send ACK packet
-        ack_packet = {'type': 'ACK', 'sequence_number': self.sequence_number, 'ack_number':self.ack_number}
-        self.socket.sendto(json.dumps(ack_packet).encode(), (data_packet['client_ip'], data_packet['client_port']))
-
-        return data_packet['data']
+        self.display_self()
+        self.display(data_packet)
+        if data_packet['sequence_number'] == self.ack_number:
+            self.display(data_packet)
+            self.ack_number = data_packet['sequence_number'] + len(data_packet['data'])  # Update acknowledgment number
+            self.sequence_number = data_packet['ack_number']
+            # Send ACK packet
+            self.flags = '00010000'  # Set ACK flag
+            ack_packet = {'type': 'ACK', 'sequence_number': self.sequence_number, 'ack_number': self.ack_number, 'flags':self.flags}
+            self.socket.sendto(json.dumps(ack_packet).encode(), (data_packet['client_ip'], data_packet['client_port']))
+            return data_packet['data']
+        else:
+            # Handle out of order
+            print("retry")
+            return False
 
     def handle_request(self, request):
         # Handle HTTP request
